0a525be9ae8c83d71acf3266411177aee3e99d38
ejbs <emiljbs@gmail.com>
2014-05-03 20:49:31 +0200

    Added ImageLoader

---
+++ src/ImageLoader.java
@@ -0,0 +1,12 @@
+public class ImageLoader {
+        public static BufferedImage readImage(String path) {
+                BufferedImage img = null;
+                try {
+                        img = ImageIO.read(new File(path));
+                } catch (IOException e) {
+                        System.err.println("Error: Could not read image +"path);
+                        System.exit(1);
+                }
+        }
+        
+}
