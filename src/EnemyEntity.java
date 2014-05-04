52f5075568de2cb08f7ed6cb38356422cf6b3694
gitoverhere <andre.v.josefsson@gmail.com>
2014-05-04 13:06:14 +0200

    Fixed indentation

--- src/EnemyEntity.java
+++ src/EnemyEntity.java
@@ -3,8 +3,8 @@ import java.awt.Graphics;
 
 
 public class EnemyEntity {
-        private final int TYPE = 1;
-        private Color color;
+    private final int TYPE = 1;
+    private Color color;
 	private boolean intersectable;
 	private double xPos;
 	private double yPos;
@@ -13,7 +13,7 @@ public class EnemyEntity {
 	private double enginePower;
 	private int width;
 	private int height;
-	
+
 	public EnemyEntity(double x, double y, int width, int height, Color c, double enginePower) {
 		this.xPos = x;
 		this.yPos = y;
@@ -24,67 +24,67 @@ public class EnemyEntity {
 		//just a starting value
 		this.enginePower = enginePower;
 	}
-	
+
 	public double getX(){
 		return xPos;
 	}
-	
+
 	public void setX(double x){
 		this.xPos = x;
 	}
-	
+
 	public double getY(){
 		return yPos;
 	}
-	
+
 	public void setY(double y){
 		this.yPos = y;
 	}
-	
+
 	public double getVelocityX(){
 		return xVel;
 	}
-	
+
 	public void setVelocityX(double x){
 		this.xVel = x;
 	}
-	
+
 	public double getVelocityY(){
 		return yVel;
 	}
-	
+
 	public void setVelocityY(double y){
 		this.yVel = y;
 	}
-	
+
 	public double getEnginePower(){
 		return enginePower;
 	}
-	
+
 	public int getWidth(){
 		return width;
 	}
-	
+
 	public int getHeight(){
 		return height;
 	}
-	
+
 	public double getWeight(){
 		return width*height;
 	}
-	
+
 	public Color getColor(){
 		return color;
 	}
-	
+
 	public boolean getIntersectable(){
 		return intersectable;
 	}
-	
+
 	public int getType(){
 		return TYPE;
 	}
-	
+
 	public void draw(Graphics g){
 		g.setColor(getColor());
 		g.fillRect( (int)getX(), (int)getY(), getWidth(), getHeight() );
