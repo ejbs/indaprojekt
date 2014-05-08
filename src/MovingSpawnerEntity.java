71581d97d603f9553154e8e9cd9cd0bc17c9187a
ejbs <emiljbs@gmail.com>
2014-05-08 20:56:27 +0200

    added docs to MovingSpawnerEntity

--- src/MovingSpawnerEntity.java
+++ src/MovingSpawnerEntity.java
@@ -1,3 +1,6 @@
+/**
+ * Every entity that moves on the screen and spawns bullets extends this class.
+ */
 public class MovingSpawnerEntity extends EnemyEntity {
         private BulletSpawner spawner;
 }
