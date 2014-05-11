97811bb4e465f813872ad887acbb66cb84ac19c9
ejbs <emiljbs@gmail.com>
2014-05-11 14:15:45 +0200

    Changed SimpleEnemy to use the FunkySpawner

--- src/SimpleEnemy.java
+++ src/SimpleEnemy.java
@@ -2,19 +2,22 @@ import java.awt.Color;
 import java.awt.Graphics;
 
 public class SimpleEnemy extends EnemyEntity {
-        ExperimentalSpawner sp;
+        FunkySpawner sp;
+        boolean first;
 
         public SimpleEnemy(int x, int y) {
                 super(x, y, 30, 30, Color.MAGENTA, 0.002, 0.1, 0.1);
-                sp =  new ExperimentalSpawner(x, y);
+                sp =  new FunkySpawner(x, y);
+                first = true;
         }
         
         public void tick() {
                 super.tick();
                 sp.setX(super.getX());
                 sp.setY(super.getY());
-                if(Math.random() > 0.9) {
+                for(int i = 0; i < 15; i++) {
                         Game.entities.add(sp.spawnBullet());
                 }
         }
 }
+
