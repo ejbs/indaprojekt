8df3456c74845d4f0c3876fa644e311ae4aff082
ejbs <emiljbs@gmail.com>
2014-05-13 19:28:01 +0200

    The indentation was wonky

--- src/BossSpawner.java
+++ src/BossSpawner.java
@@ -10,9 +10,9 @@ public class BossSpawner implements BulletSpawner {
 
 	public EnemyEntity spawnBullet(double x, double y, int width, int height){
 		this.currentStep++;
-		                return new EnemyEntity(x+(width/2), y+(height/2), 10, 10, Color.GREEN, 0.00001,
-		                                       Math.cos(step*currentStep)/3,
-		                                       Math.sin(step*currentStep)/3,
+                return new EnemyEntity(x+(width/2), y+(height/2), 10, 10, Color.GREEN, 0.00001,
+                                       Math.cos(step*currentStep)/3,
+                                       Math.sin(step*currentStep)/3,
                                        null);
 	}
 
