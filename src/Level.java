240631a723badb365e9fbccdcc5ae4ea6f0cc893
ejbs <emiljbs@gmail.com>
2014-05-14 13:53:55 +0200

    wrote sorting method for the nodes

--- src/Level.java
+++ src/Level.java
@@ -3,8 +3,33 @@
  * 
  */
 public class Level {
-        ArrayList<LevelNodes> levelNodes;
+        ArrayList<LevelNode> levelNodes;
 
+        public Level(ArrayList<LevelNode> levelNodes) {
+                this.levelNodes = levelNodes;
+                sortNodesByActivationDifficulty(this.levelNodes);
+        }
+        
+        public void sortNodesByActivationDifficulty(ArrayList<LevelNode> nodes) {
+                Collections.sort(nodes, new Comparator<LevelNode>() {
+                                public int compare(LevelNode n1, LevelNode n2) {
+                                        int a1 = n1.getActivationDifficulty();
+                                        int a2 = n2.getActivationDifficulty();
+
+                                        if(a1 == a2) {
+                                                return 0;
+                                        }
+                                        if(a1 > a2) {
+                                                return 1;
+                                        }
+                                        else {
+                                                return -1;
+                                        }
+                                }
+                        });
+        }
+        
+        
         public addSimpleNode(int activationDifficulty, ArrayList<EnemyEntity> spawnThese) {
                 
         }
