1a226ace888620710fbf059777dce08130744d07
ejbs <emiljbs@gmail.com>
2014-05-14 13:45:37 +0200

    Initial Level class development. Implementation of levels may change

---
+++ src/Level.java
@@ -0,0 +1,11 @@
+/**
+ * Introduces the concept of a level to the game.
+ * 
+ */
+public class Level {
+        ArrayList<LevelNodes> levelNodes;
+
+        public addSimpleNode(int activationDifficulty, ArrayList<EnemyEntity> spawnThese) {
+                
+        }
+}
