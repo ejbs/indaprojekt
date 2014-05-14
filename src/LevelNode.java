633ad1a0c0ad25ecab787d66053dacc133af6a43
ejbs <emiljbs@gmail.com>
2014-05-14 14:09:54 +0200

    Realised that an interface can't have some of the stuff that I defined in the beginning. Decided to scrap that stuff instead of turning it into a class.

--- src/LevelNode.java
+++ src/LevelNode.java
@@ -1,3 +1,5 @@
+import java.util.*;
+
 /**
  * This is the LevelNode.
  * A LevelNode is something which is activated by a Level at some activationDifficulty.
@@ -5,13 +7,6 @@
  * Activation at the appropriate activationDifficulty is not enforced however it is expected.
  */
 public interface LevelNode {
-        private int final activationDifficulty;
-
-        /**
-         * The activationDifficulty is the difficulty at which activate() should be called.
-         * spawnThese are the entities which are to be spawned.
-         */
-        public LevelNode(int activationDifficulty, ArrayList<EnemyEntity> spawnThese);
         
         public int getActivationDifficulty();
 
