86afd634619d03b986f2277a21cedd7d784b5ba3
ejbs <emiljbs@gmail.com>
2014-05-14 21:14:37 +0200

    blahblah levellevel

--- src/Game.java
+++ src/Game.java
@@ -272,18 +272,23 @@ public class Game implements KeyListener{
 		difficulty = 80;
 		readHighscore();
 
+                Random r = new Random();
+
                 ArrayList<LevelNode> nodes = new ArrayList<LevelNode>();
                 ArrayList<EnemyEntity> en0 = new ArrayList<EnemyEntity>();
                 ArrayList<EnemyEntity> en1 = new ArrayList<EnemyEntity>();
                 ArrayList<EnemyEntity> en2 = new ArrayList<EnemyEntity>();
 
+                
                 en0.add(new EnemyEntity(1500,400,100,100,Color.PINK,0.001,-0.2,0,new RingSpawner()));
                 en1.add(new EnemyEntity(1500,400,100,100,Color.PINK,0.001,-0.2,0,new RingSpawner()));
                 en1.add(new EnemyEntity(WIDTH/2, 0,100,100,Color.PINK,0.001,0,0.2,new RingSpawner()));
                 en1.add(new EnemyEntity(WIDTH/2,HEIGHT,100,100,Color.PINK,0.001,0,-0.2,new RingSpawner()));
-                for(int i = 0; i < 3; i++) {
-                        en2.add(new EnemyEntity(Math.random()*500,0,25,25,Color.MAGENTA, 0.001, Math.random(), Math.random(), new FunkySpawner()));
+                for(int i = 0; i < 2; i++) {
+                        en2.add(new EnemyEntity(r.nextInt(WIDTH-300),0,25,25,Color.MAGENTA, 0.001, 0.2, 0.3, new ExperimentalSpawner()));
                 }
+                en2.add(new EnemyEntity(r.nextInt(WIDTH-300),0,25,25,Color.MAGENTA, 0.001, 0.2, 0.3, new FunkySpawner()));
+                
                 nodes.add(new SimpleLevelNode(70, en0));
                 nodes.add(new SimpleLevelNode(60, en1));
                 nodes.add(new SimpleLevelNode(50, en2));
