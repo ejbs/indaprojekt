c617ed00752ed84d90642db95424b68d549703ac
ejbs <emiljbs@gmail.com>
2014-05-15 21:03:27 +0200

    Wrote some docs for BulletSpawner

--- src/BulletSpawner.java
+++ src/BulletSpawner.java
@@ -1,5 +1,10 @@
 import java.util.ArrayList;
 
+/**
+ * This interface is used by all spawners except SimpleSpawner (special case).
+ * Implement this interface if you want to make a class which spawns bullets
+ * and you want to use it with an EnemyEntity.
+ */
 public interface BulletSpawner {
 
 	/**
