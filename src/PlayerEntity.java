64845d90b05a032814f5d9e3df953e62cfd60bf5
gitoverhere <andre.v.josefsson@gmail.com>
2014-05-15 21:00:47 +0200

    Added documentation for PlayerEntity

--- src/PlayerEntity.java
+++ src/PlayerEntity.java
@@ -2,122 +2,178 @@ import java.awt.*;
 import java.util.ArrayList;
 
 /**
- * This is the PlayerEntity.
- * It is the entity that the player controls on the screen.
- */
+* This is the PlayerEntity.
+* It is the entity that the player controls on the screen.
+*/
 public class PlayerEntity implements ScreenEntity {
-	//The type of a PlayerEntity is always 0.
-	private final int TYPE = 0;
-	private Color color;
-	private boolean intersectable;
-	private double xPos;
-	private double yPos;
-	private double xVel;
-	private double yVel;
-	private double enginePower;
-	private int width;
-	private int height;
+    //The type of a PlayerEntity is always 0.
+    private final int TYPE = 0;
+    private Color color;
+    private boolean intersectable;
+    private double xPos;
+    private double yPos;
+    private double xVel;
+    private double yVel;
+    private double enginePower;
+    private int width;
+    private int height;
 
-	public PlayerEntity(double x, double y, int width, int height, Color c) {
-		this.xPos = x;
-		this.yPos = y;
-		this.width = width;
-		this.height = height;
-		this.color = c;
-		this.intersectable = true;
-		this.enginePower = 0.002;
-	}
+    /**
+    * Returns a PlayerEntity with the specified properties.
+    */
+    public PlayerEntity(double x, double y, int width, int height, Color c) {
+        this.xPos = x;
+        this.yPos = y;
+        this.width = width;
+        this.height = height;
+        this.color = c;
+        this.intersectable = true;
+        this.enginePower = 0.002;
+    }
 
-	public double getX(){
-		return xPos;
-	}
+    /**
+    * @inheritDoc
+    */
+    public double getX(){
+        return xPos;
+    }
 
-	public void setX(double x){
-		this.xPos = x;
-	}
+    /**
+    * @inheritDoc
+    */
+    public void setX(double x){
+        this.xPos = x;
+    }
 
-	public double getY(){
-		return yPos;
-	}
+    /**
+    * @inheritDoc
+    */
+    public double getY(){
+        return yPos;
+    }
 
-	public void setY(double y){
-		this.yPos = y;
-	}
+    /**
+    * @inheritDoc
+    */
+    public void setY(double y){
+        this.yPos = y;
+    }
 
-	public double getVelocityX(){
-		return xVel;
-	}
+    /**
+    * @inheritDoc
+    */
+    public double getVelocityX(){
+        return xVel;
+    }
 
-	public void setVelocityX(double x){
-		this.xVel = x;
-	}
+    /**
+    * @inheritDoc
+    */
+    public void setVelocityX(double x){
+        this.xVel = x;
+    }
 
-	public double getVelocityY(){
-		return yVel;
-	}
+    /**
+    * @inheritDoc
+    */
+    public double getVelocityY(){
+        return yVel;
+    }
 
-	public void setVelocityY(double y){
-		this.yVel = y;
-	}
+    /**
+    * @inheritDoc
+    */
+    public void setVelocityY(double y){
+        this.yVel = y;
+    }
 
-	public double getEnginePower(){
-		return enginePower;
-	}
+    /**
+    * @inheritDoc
+    */
+    public double getEnginePower(){
+        return enginePower;
+    }
 
-	public int getWidth(){
-		return width;
-	}
+    /**
+    * @inheritDoc
+    */
+    public int getWidth(){
+        return width;
+    }
 
-	public int getHeight(){
-		return height;
-	}
+    /**
+    * @inheritDoc
+    */
+    public int getHeight(){
+        return height;
+    }
 
-	public double getWeight(){
-		return width*height;
-	}
+    /**
+    * @inheritDoc
+    */
+    public double getWeight(){
+        return width*height;
+    }
 
-	public Color getColor(){
-		return color;
-	}
+    /**
+    * @inheritDoc
+    */
+    public Color getColor(){
+        return color;
+    }
 
-	public boolean isIntersectable(){
-		return intersectable;
-	}
+    /**
+    * @inheritDoc
+    */
+    public boolean isIntersectable(){
+        return intersectable;
+    }
 
-	public int getType(){
-		return TYPE;
-	}
+    /**
+    * @inheritDoc
+    */
+    public int getType(){
+        return TYPE;
+    }
 
-	public void addSpawner(BulletSpawner bs){
-	}
+    /**
+    * @inheritDoc
+    */
+    public void addSpawner(BulletSpawner bs){
+    }
 
-	public ArrayList<EnemyEntity> spawnBullet(){
-		return null;
-	}
+    /**
+    * @inheritDoc
+    */
+    public ArrayList<EnemyEntity> spawnBullet(){
+        return null;
+    }
 
-	public void draw(Graphics g){
-		g.setColor(getColor());
-		g.fillRect( (int)getX(), (int)getY(), getWidth(), getHeight() );
-	}
+    /**
+    * @inheritDoc
+    */
+    public void draw(Graphics g){
+        g.setColor(getColor());
+        g.fillRect( (int)getX(), (int)getY(), getWidth(), getHeight() );
+    }
 
-        /**
-         * tick() checks for any relevant keys that the player might've pressed and
-         * adjust its velocity and position accordingly
-         */
-        public void tick() {
+    /**
+    * @inheritDoc
+    */
+    public void tick() {
         if(Game.keys.get("up")){
-			this.setVelocityY(this.getVelocityY()-this.getEnginePower());
-		}
-		if(Game.keys.get("down")){
-			this.setVelocityY(this.getVelocityY()+this.getEnginePower());
-		}
-		if(Game.keys.get("left")){
-			this.setVelocityX(this.getVelocityX()-this.getEnginePower());
-		}
-		if(Game.keys.get("right")){
-			this.setVelocityX(this.getVelocityX()+this.getEnginePower());
-		}
-                this.xPos += this.xVel;
-		this.yPos += this.yVel;
-	}
+            this.setVelocityY(this.getVelocityY()-this.getEnginePower());
+        }
+        if(Game.keys.get("down")){
+            this.setVelocityY(this.getVelocityY()+this.getEnginePower());
+        }
+        if(Game.keys.get("left")){
+            this.setVelocityX(this.getVelocityX()-this.getEnginePower());
+        }
+        if(Game.keys.get("right")){
+            this.setVelocityX(this.getVelocityX()+this.getEnginePower());
+        }
+        this.xPos += this.xVel;
+        this.yPos += this.yVel;
+    }
 }
